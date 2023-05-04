08dd21f0b5c58d4d1841c1cd08035b831d1b4b07
abdurasulov-olqar <abdurasulovolqar123@gmail.com>
2023-05-04 16:48:21 +0500

    add echo funktion

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from telegram.ext import CallbackContext
 def start(update: Update, context: CallbackContext):
     update.message.reply_text('Hello, World!')
 
+def echo(update: Update, context: CallbackContext):
+    text = update.message.text
+    update.message.reply_text(text)   
+
